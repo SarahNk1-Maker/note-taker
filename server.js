bfbdde5605790433f9d29bbc8e8f2d188ac393b2
SarahNk1-Maker <sarahnaveed216@gmail.com>
2023-09-11 19:32:30 -0400

    new change

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs =require("fs");
 const api_routers =require('./routers/api-routers');
 const html_routers =require('./routers/html-routers');
 
-const PORT = process.env.PORT||3001;
+const PORT = process.env.PORT;
 
 const app = express();
 
